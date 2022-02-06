8568e1c6ee08d53e9bfa06e84150bdad556cff2b
Artem Pavlenko <artempavlenko1703@gmail.com>
2022-02-06 14:18:26 +0300

    slider

--- js/script.js
+++ js/script.js
@@ -175,34 +175,22 @@ window.addEventListener('DOMContentLoaded', () => {
 		}
 	}
 
-	new MenuCard(
-		"img/tabs/vegy.jpg",
-		"vegy",
-		'Меню "Фитнес"',
-		'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и	фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким	качеством!',
-		9,
-		'.menu .container'
-	).render();
+	const getResources = async (url) => {
+		const res = await fetch(url);
 
-	new MenuCard(
-		"img/tabs/elite.jpg",
-		"elite",
-		'Меню “Премиум”',
-		'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-		14,
-		'.menu .container',
-		'menu__item'
-	).render();
+		if (!res.ok) {
+			throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+		}
 
-	new MenuCard(
-		"img/tabs/post.jpg",
-		"post",
-		'Меню "Постное"',
-		'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов	животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет	тофу и импортных вегетарианских стейков.',
-		21,
-		'.menu .container',
-		'menu__item'
-	).render();
+		return await res.json();
+	}
+
+	getResources('http://localhost:3000/menu')
+		.then(data => {
+			data.forEach(({ img, altimg, title, descr, price }) => {
+				new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+			})
+		})
 
 	// Forms
 
@@ -217,6 +205,18 @@ window.addEventListener('DOMContentLoaded', () => {
 	forms.forEach(item => {
 		bindPostData(item);
 	});
+
+	const postData = async (url, data) => {
+		const res = await fetch(url, {
+			method: "POST",
+			headers: {
+				'Content-type': 'application/json'
+			},
+			body: data
+		});
+
+		return await res.json();
+	}
 
 	function bindPostData(form) {
 		form.addEventListener('submit', (e) => {
@@ -233,19 +233,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
 			const formData = new FormData(form);
 
-			const object = {};
-			formData.forEach(function (value, key) {
-				object[key] = value;
-			});
+			const json = JSON.stringify(Object.fromEntries(formData.entries()))
 
-			fetch('server.php', {
-				method: "POST",
-				headers: {
-					'Content-type': 'application/json'
-				},
-				body: JSON.stringify(object)
-			})
-				.then(data => data.text())
+			postData('http://localhost:3000/requests', json)
 				.then(data => {
 					console.log(data);
 					showThanksModal(message.success);
@@ -285,7 +275,122 @@ window.addEventListener('DOMContentLoaded', () => {
 		}, 4000)
 	}
 
-	fetch('http://localhost:3000/menu')
-		.then(data => data.json())
-		.then(res => console.log(res))
+	// slider
+
+	const slides = document.querySelectorAll('.offer__slide'),
+		slider = document.querySelector('.offer__slider'),
+		prev = document.querySelector('.offer__slider-prev'),
+		next = document.querySelector('.offer__slider-next'),
+		total = document.querySelector('#total'),
+		current = document.querySelector('#current'),
+		slidesWrapper = document.querySelector('.offer__slider-wrapper'),
+		slidesField = document.querySelector('.offer__slider-inner'),
+		width = window.getComputedStyle(slidesWrapper).width;
+
+	let slideIndex = 1,
+		offset = 0;
+
+	if (slides.length < 10) {
+		total.textContent = `0${slides.length}`;
+		current.textContent = `0${slideIndex}`;
+	} else {
+		total.textContent = slides.length;
+		current.textContent = slideIndex;
+	}
+
+	slidesField.style.width = 100 * slides.length + '%';
+	slidesField.style.display = 'flex';
+	slidesField.style.transition = '0.5s all';
+
+	slidesWrapper.style.overflow = 'hidden';
+
+	slides.forEach(slide => {
+		slide.style.width = width
+	})
+
+	slider.style.position = 'relative';
+
+	const indicators = document.createElement('ol'),
+		dots = [];
+	indicators.classList.add('carousel-indicators');
+	slider.append(indicators);
+
+	for (let i = 0; i < slides.length; i++) {
+		const dot = document.createElement('li');
+		dot.classList.add('dot');
+		dot.setAttribute('data-slide-to', i + 1);
+		if (i == 0) {
+			dot.style.opacity = 1;
+		}
+		indicators.append(dot);
+		dots.push(dot);
+	}
+
+	function deleteNotDigits(str) {
+		return +str.replace(/\D/g, '')
+	}
+
+	next.addEventListener('click', (e) => {
+		e.preventDefault();
+		if (offset == deleteNotDigits(width) * (slides.length - 1)) {
+			offset = 0;
+			slideIndex -= slides.length - 1;
+		} else {
+			offset += deleteNotDigits(width);
+			slideIndex += 1;
+		}
+
+		dotIndex();
+
+		slidesField.style.transform = `translateX(-${offset}px)`;
+		showIndex(slideIndex);
+	});
+
+	prev.addEventListener('click', (e) => {
+		e.preventDefault();
+		if (offset == 0) {
+			offset = deleteNotDigits(width) * (slides.length - 1);
+			slideIndex = slides.length
+		} else {
+			offset -= deleteNotDigits(width);
+			slideIndex -= 1;
+		}
+
+		dotIndex();
+
+		slidesField.style.transform = `translateX(-${offset}px)`;
+		showIndex(slideIndex);
+	});
+
+	function showIndex(n) {
+		if (n < 10) {
+			current.textContent = `0${slideIndex}`;
+		} else {
+			current.textContent = `${slideIndex}`;
+		}
+	}
+
+	function dotIndex() {
+		dots.forEach(item => item.style.opacity = '0.5');
+		dots[slideIndex - 1].style.opacity = '1'
+	}
+
+	dots.forEach((item, i) => {
+		item.addEventListener('click', (e) => {
+			// const slideTo = e.target.getAttribute('data-slide-to');
+
+			// slideIndex = slideTo;
+			// offset = deleteNotDigits(width) * (slideTo - 1);
+			// slidesField.style.transform = `translateX(-${offset}px)`;
+			// showIndex(slideIndex);
+			// dotIndex();
+
+
+			offset = deleteNotDigits(width) * i;
+			slidesField.style.transform = `translateX(-${offset}px)`;
+			slideIndex = i + 1;
+			showIndex(slideIndex);
+			dotIndex();
+		});
+	});
 });
